3b3f7d7e94ee06f50d5598c7bc54ce847ff661a2
TJ LaGrow <tjseeker@gmail.com>
2015-11-06 12:05:35 -0800

    Just a test to push...?
    (Still trying to get the api figured out)

--- c-in-mips.c
+++ c-in-mips.c
@@ -4,6 +4,8 @@
 #include <ctype.h>
 #include <math.h>
 
+// The first and (probably) only comment.... :DDD
+
 static int RegisterFile[31] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 static char* RegisterFileNames[31] = {'$v0', '$v1', '$a0', '$a1', '$a2', '$a3', '$t0', '$t1', '$t2', '$t3', '$t4', '$t5', '$t6', '$t7', '$s0', '$s1', '$s2', '$s3', '$s4', '$s5', '$s6', '$s7', '$t8', '$t9', '$gp', '$sp', '$fp', '$ra'};
 static int data_memory[1024];
@@ -14,3 +16,4 @@ int main() {
 
 
 }
+
