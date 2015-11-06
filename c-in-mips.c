7b883d782a5e00d2ed149ebbded18becea4625db
Jacob Bieker <jacob@bieker.tech>
2015-11-05 16:38:48 -0800

    Add register character names

--- c-in-mips.c
+++ c-in-mips.c
@@ -5,6 +5,7 @@
 #include <math.h>
 
 static int RegisterFile[32] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+static char* RegisterFileNames[33] = { '$0', '$v0', '$v1', '$a0', '$a1', '$a2', '$a3', '$t0', '$t1', '$t2', '$t3', '$t4', '$t5', '$t6', '$t7', '$s0', '$s1', '$s2', '$s3', '$s4', '$s5', '$s6', '$s7', '$t8', '$t9', '$gp', '$sp', '$fp', '$ra', '$zero'};
 static int data_memory[1024];
 static char* string_memory[2014];
 static char* instructions[100];
