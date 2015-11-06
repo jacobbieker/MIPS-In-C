346b74ecd496bec6d2587f227b251229214795c5
Jacob Bieker <jacob@bieker.tech>
2015-11-05 16:12:34 -0800

    Initial Commit
    RegisterFile array created

---
+++ c-in-mips.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+static char RegisterFile [32] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
