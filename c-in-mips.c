1c2763e1b1b8494e69f9b9ca0466c155bfc3d70e
matthewjagielski <mcj.uoregon.edu>
2015-11-06 12:17:15 -0800

    double quotes
    characters are single, strings are double

--- c-in-mips.c
+++ c-in-mips.c
@@ -5,12 +5,30 @@
 #include <math.h>
 
 static int RegisterFile[31] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-static char* RegisterFileNames[31] = {'$v0', '$v1', '$a0', '$a1', '$a2', '$a3', '$t0', '$t1', '$t2', '$t3', '$t4', '$t5', '$t6', '$t7', '$s0', '$s1', '$s2', '$s3', '$s4', '$s5', '$s6', '$s7', '$t8', '$t9', '$gp', '$sp', '$fp', '$ra'};
+static char RegisterFileNames[31][4] = {"$v0", "$v1", "$a0", "$a1", "$a2", "$a3", "$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7", "$s0", "$s1", "$s2", "$s3", "$s4", "$s5", "$s6", "$s7", "$t8", "$t9", "$gp", "$sp", "$fp", "$ra"};
 static int data_memory[1024];
 static char* string_memory[2014];
 static char* instructions[1000000];
 
 int main() {
 
+    return 0;
+}
 
+int readmips(char* filename){
+    FILE * assembly = fopen(filename, "r");
+    int i=0;
+    if (assembly == 0)
+    {
+        //fopen returns 0, the NULL pointer, on failure
+        perror("Cannot open input file\n");
+        exit(-1);
+    }
+    else
+    {
+        while(fscanf(assembly,"%[#\n]",instructions[i])!=EOF){
+            i++;
+            printf("%s", instructions[i]);
+        }
+    }
 }
