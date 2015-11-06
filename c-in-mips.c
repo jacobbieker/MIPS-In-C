b94b237f9fafeee0c80401f12f53988024b1ffa2
Jacob Bieker <jacob@bieker.tech>
2015-11-05 16:23:07 -0800

    Add initial memory array

--- c-in-mips.c
+++ c-in-mips.c
@@ -6,3 +6,8 @@
 
 static int RegisterFile[32] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
+int main() {
+
+	int data_memory[1024];
+	char string_memeory[2014];
+}
