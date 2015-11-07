0fbd32a107b32d30c0a1683f6ed375bea64fb6b9
matthewjagielski <mcj.uoregon.edu>
2015-11-07 13:54:58 -0800

    instruction_types
    starting to be able to decode instructions!

--- c-in-mips.c
+++ c-in-mips.c
@@ -8,11 +8,47 @@ static int RegisterFile[31] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
 static char RegisterFileNames[31][4] = {"$v0", "$v1", "$a0", "$a1", "$a2", "$a3", "$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7", "$s0", "$s1", "$s2", "$s3", "$s4", "$s5", "$s6", "$s7", "$t8", "$t9", "$gp", "$sp", "$fp", "$ra"};
 static int data_memory[1024];
 static char* string_memory[1024];
-static char* instructions[1000000];
+static char* instructions[1024];
+static char** jump_names; 
+static int* jump_locations;
+typedef enum {add, addu, sub, subu, addi, addiu, mult, multu, Div, divu, lw, lh, lhu, lb, lbu, sw, sh, sb, lui, mfhi, mflo, mfcZ, mtcZ, and, andi, or, ori, xor, nor, slt, sltu, slti, sll, srl, sra, sllv, srlv, srav, beq, bne, j, jr, jal} instruction;
+
+static char* instr_name_list[46] = {"add", "addu", "sub", "subu", "addi", "addiu", "mult", "multu", "div", "divu", "lw", "lh", "lhu", "lb", "lbu", "sw", "sh", "sb", "lui", "mfhi", "mflo", "mfcZ", "mtcZ", "and", "andi", "or", "ori", "xor", "nor", "slt", "sltu", "slti", "sll", "srl", "sra", "sllv", "srlv", "srav", "beq", "bne", "j", "jr", "jal"};
+
+
+struct Instruction_Type {
+    instruction instr;
+};
+
+struct r_type {
+    instruction instr;
+    char dest_register;
+    char s_register1;
+    char s_register2;
+    char shamt;
+};
+
+struct i_type {
+    instruction instr;
+    char dest_register;
+    char s_register;
+    int immediate;
+};
+
+struct j_type{
+    instruction instr;
+    int jump_to;
+};
+
+struct Instruction_Type* decodeinstruction(char* instr);
 
 int main() {
     int i=0;
-    readmips("bubble.asm");
+    struct Instruction_Type* a_ptr = decodeinstruction("0");
+    struct j_type a;
+    a = *((struct j_type*) a_ptr);
+    printf("%d %d\n", a.instr, a.jump_to);
+    /*readmips("bubble.asm");
     while(instructions[i]!=NULL){
         printf("%s\n",instructions[i]);
         ++i;
@@ -20,13 +56,13 @@ int main() {
     while(instructions[i]!=NULL){
         free(instructions[i]);
         i++;
-    }
+    }*/
     return 0;
 }
 
 int readmips(char* filename){
     FILE * assembly = fopen(filename, "r");
-    int lineno=0; int charno=0; int i =0; int spacebreak=0;
+    int lineno=0; int charno=0; int i=0; int spacebreak=0;
     char buffer[256];
     char* temp;
     if (assembly == 0)
@@ -51,122 +87,37 @@ int readmips(char* filename){
                 *(temp+i-spacebreak)=buffer[i];
                 i++;
             }
-            //printf("%s\n",temp);
-            instructions[lineno]=temp;
-            if (charno!=0){
-                lineno++;
+            if (charno!=0 && spacebreak!=charno){
+                instructions[lineno]=temp;
+                lineno++; //rewrite over memory if the line has no instruction
             }
         }
     }
 }
 
-// Start of Arithmetic functions
-//TODO: Mult and Divide, all operations with 'u' have to use unsigned values, others ahve to check for overflow
-void add(register1, register2, register3) {
-	register1 = register2 + register3;
+
+char* getinstruction(int pc){
+    return instructions[pc];
 }
 
-void addu(register1, register2, register3) {
-	register1 = register2 + register3;
+int controllogic(){
+    int pc=0;
+    char *cur_instruction;
+    struct j_type j_instr;
+    struct i_type i_instr;
+    struct r_type r_instr;
+    struct Instruction_Type instr;
+    cur_instruction = getinstruction(pc);
+    instr = *decodeinstruction(cur_instruction);
+    //alu
+    //memory read/write
+    //register read/write
+    return 0;
+
 }
 
-void sub(register1, register2, register3) {
-	register1 = register2 - register3;
-}
-
-void subu(register1, register2, register3) {
-	register1 = register2 - register3;
-}
-
-void addi(register1, register2, number) {
-	register1 = register2 + number;
-}
-
-void addiu(register1, register2, number) {
-	register1 = register2 + number;
-}
-
-//Mult and Divide left, have to use LO and HI
-
-// Start of Logical functions
-
-void and(register1, register2, register3) {
-	if (register2 == 1 && register3 == 1) {
-		register1 = 1;
-	}
-	else {
-		register1 = 0;
-	}
-}
-
-void andi(register1, register2, number) {
-	if (register2 == 1 && number == 1) {
-		register1 = 1;
-	}
-	else {
-		register1 = 0;
-	}
-}
-
-void or (register1, register2, register3) {
-	if (register2 == 1 || register3 == 1) {
-		register1 = 1;
-	}
-	else {
-		register1 = 0;
-	}
-}
-
-void ori(register1, register2, number) {
-	if (register2 == 1 || number == 1) {
-		register1 = 1;
-	}
-	else {
-		register1 = 0;
-	}
-}
-
-void xor(register1, register2, register3) {
-	if ((register2 == 1 && register3 != 1) || (register2 != 1 && register3 == 1)) {
-		register1 = 1;
-	}
-	else {
-		register1 = 0;
-	}
-}
-
-void nor(register1, register2, register3) {
-	if (!(register2 == 1 || register3 == 1)) {
-		register1 = 1;
-	}
-	else {
-		register1 = 0;
-	}
-}
-
-void slt(register1, register2, register3) {
-	if (register2 < register3) {
-		register1 = 1;
-	}
-	else {
-		register1 = 0;
-	}
-}
-
-void sltu(register1, register2, register3) {
-	if (register2 < register3) {
-		register1 = 1;
-	}
-	else {
-		register1 = 0;
-	}
-}
-
-void slti(register1, register2, number) {
-	if (register2 < number) {
-		register1 = 1;
-	}
-	else {
-		register1 = 0;
-	}
+struct Instruction_Type* decodeinstruction(char* instr){
+    struct j_type a={j, 100123};
+    struct Instruction_Type* ptr = (struct Instruction_Type*) &a;
+    return ptr;
 }
