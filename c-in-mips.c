b0eabce9db4c7fe4e94e0bca177e90a760bff8c3
matthewjagielski <mcj.uoregon.edu>
2015-11-08 00:58:19 -0800

    parsing and decoding instructions now works
    decodeinstruction() decodes a char* instruction
    lots of helper functions to expedite this process
    the instruction name enum has been reordered along with the
    corresponding string array
    lots of cool new structs for holding instruction values
    header comments
    it compiles and even runs!
    handles even the weird instructions, like indexed registers in sw or how
    jr, mflo, and mfhi are r type instructions with only one register
    zero register is indicated by a register number of 32
    mallocs are all accounted for in america()

--- c-in-mips.c
+++ c-in-mips.c
@@ -4,25 +4,25 @@
 #include <ctype.h>
 #include <math.h>
 
-// The last 0 in the RegisterFile is for the registers $0, $zero
-static int RegisterFile[32] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+static int RegisterFile[31] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 static char RegisterFileNames[31][4] = {"$v0", "$v1", "$a0", "$a1", "$a2", "$a3", "$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7", "$s0", "$s1", "$s2", "$s3", "$s4", "$s5", "$s6", "$s7", "$t8", "$t9", "$gp", "$sp", "$fp", "$ra"};
-static int data_memory[1024];
-static char* string_memory[1024];
-static char* instructions[1024];
-static char** jump_names; 
-static int* jump_locations;
 
-typedef enum {add, addu, sub, subu, addi, addiu, mult, multu, Div, divu, lw, lh, lhu, lb, lbu, sw, sh, sb, lui, mfhi, mflo, mfcZ, mtcZ, and, andi, or, ori, xor, nor, slt, sltu, slti, sll, srl, sra, sllv, srlv, srav, beq, bne, j, jr, jal} instruction;
+static int data_memory[1024]; // int values
+static char* string_memory[1024]; // string values
+static char* instructions[1024]; // instructions
 
-static char* instr_name_list[46] = {"add", "addu", "sub", "subu", "addi", "addiu", "mult", "multu", "div", "divu", "lw", "lh", "lhu", "lb", "lbu", "sw", "sh", "sb", "lui", "mfhi", "mflo", "mfcZ", "mtcZ", "and", "andi", "or", "ori", "xor", "nor", "slt", "sltu", "slti", "sll", "srl", "sra", "sllv", "srlv", "srav", "beq", "bne", "j", "jr", "jal"};
+static char* jump_names[128]; // jump location names and addresses
+static int jump_locations[128];
 
+// instruction name enum and array
+typedef enum {add, addu, sub, subu, and, or, nor, xor, slt, sltu, srlv, sllv, srav, sll, srl, sra, mult, multu, Div, divu, mfcZ, mtcZ, mfhi, mflo, jr, addi, addiu, andi, ori, slti, beq, bne, lw, lh, lhu, lb, lbu, sw, sh, sb, lui, j, jal} instruction;
+static char* instr_name_list[44] = {"add", "addu", "sub", "subu", "and", "or", "nor", "xor", "slt", "sltu", "srlv", "sllv", "srav", "sll", "srl", "sra", "mult", "multu", "div", "divu", "mfcZ", "mtcZ", "mfhi", "mflo", "jr", "addi", "addiu", "andi", "ori", "slti", "beq", "bne", "lw", "lh", "lhu", "lb", "lbu", "sw", "sh", "sb", "lui", "j", "jal"};
 
-struct Instruction_Type {
+struct Instruction_Type { // generic instruction type - holds instruction only
     instruction instr;
 };
 
-struct r_type {
+struct r_type { // holds all r type values
     instruction instr;
     char dest_register;
     char s_register1;
@@ -30,43 +30,116 @@ struct r_type {
     char shamt;
 };
 
-struct i_type {
+struct i_type { // holds all i type values
     instruction instr;
     char dest_register;
     char s_register;
     int immediate;
 };
 
-struct j_type{
+struct j_type{ // holds all j type values
     instruction instr;
     int jump_to;
 };
 
+struct indexed_register { //for modular parsing of indexed registers (return both index and register)
+    char mem_register;
+    int index;
+};
+
+/* readmips(char*) reads the assembly file specified by filename into the instructions data array*/
+void readmips(char* filename);
+
+/*getinstruction(int pc) is an interface for the control unit to access the instructions stored
+in memory, by index specified by pc*/
+char* getinstruction(int pc);
+
+/*decodeinstruction(char*) takes an assembly instruction specified by instr and converts to an
+Instruction_Type, converting the function, any registers, and any immediate values to integers
+that are easier to operate on*/
 struct Instruction_Type* decodeinstruction(char* instr);
 
+/*make_r_type(int, char*, int*) takes the instruction specified by instr, and converts the remainder
+of the char* to an r_type struct to hold all the data an r_type can have. instrnum holds the enum
+value of the instruction*/
+struct r_type* make_r_type(int instrnum, char* instr, int*);
+
+/*make_i_type(int, char*, int*) takes the instruction specified by instr, and converts the remainder
+of the char* to an i_type struct to hold all the data an i_type can have. instrnum holds the enum
+value of the instruction*/
+struct i_type* make_i_type(int instrnum, char* instr, int*);
+
+/*make_j_type(int, char*, int*) takes the instruction specified by instr, and converts the remainder
+of the char* to a j_type struct to hold all the data a j_type can have. instrnum holds the enum
+value of the instruction*/
+struct j_type* make_j_type(int instrnum, char* instr, int*);
+
+/*nextindexedregister(char*, int*) returns the next indexed register's register and index values from
+the instruction specified by instr at the location specified by *ptr */
+struct indexed_register* nextindexedregister(char* instr, int* ptr);
+
+/*nextregister(char*, int*) returns the value of the next register in the instruction specified by
+instr, past the location specified by *ptr */
+char nextregister(char* instr, int* ptr);
+
+/*nextint(char*, int*) returns the next integer found in the instruction specified by instr, past the
+location specified by *ptr */
+int nextint(char* instr,int* ptr);
+
+/*america() frees all memory that has been malloc'ed and not otherwise freed
+IF YOU HAVE CALLED malloc() MAKE SURE TO FREE THAT MEMORY. DO IT HERE IF THERE
+IS NO BETTER PLACE. Thanks.
+Current memory that is deallocated here:
+instructions: the array holding all instructions in char* form
+jump_names: the array holding all jump names in char* form
+*/
+void america();
+
 int main() {
-    int i=0;
-    struct Instruction_Type* a_ptr = decodeinstruction("0");
-    struct j_type a;
-    a = *((struct j_type*) a_ptr);
-    printf("%d %d\n", a.instr, a.jump_to);
-    /*readmips("bubble.asm");
+    int i=0; char *a="jr $ra";
+    printf("%s\n", a); decodeinstruction(a);
+    a="add $v0 $v1 $a0";
+    printf("%s\n", a); decodeinstruction(a);
+    a="mult $a1 $a2";
+    printf("%s\n", a); decodeinstruction(a);
+    a="sll $a3 $t0 4";
+    printf("%s\n", a); decodeinstruction(a);
+    a="addi $t1 $t2 -2";
+    printf("%s\n", a); decodeinstruction(a);
+    a="lhu $t3 12($t4)";
+    printf("%s\n", a); decodeinstruction(a);
+    a="lui $sp 1245";
+    printf("%s\n", a); decodeinstruction(a);
+    a="j -123";
+    printf("%s\n", a); decodeinstruction(a);
+    a="jal 10000";
+    printf("%s\n", a); decodeinstruction(a);
+    a="lw $t5 -4($fp)";
+    printf("%s\n", a); decodeinstruction(a);
+    a="sw $t6 0($s0)";
+    printf("%s\n", a); decodeinstruction(a);
+    a="sub $gp $s7 $t8";
+    printf("%s\n", a); decodeinstruction(a);
+    
+    readmips("bubble.asm");
     while(instructions[i]!=NULL){
         printf("%s\n",instructions[i]);
         ++i;
     }
-    while(instructions[i]!=NULL){
-        free(instructions[i]);
+    i=0;
+    while(jump_names[i]!=NULL){
+        printf("%d, %s\n", jump_locations[i], jump_names[i]);
         i++;
-    }*/
+    }
+    america();
     return 0;
 }
 
-int readmips(char* filename){
+void readmips(char* filename){
     FILE * assembly = fopen(filename, "r");
-    int lineno=0; int charno=0; int i=0; int spacebreak=0;
-    char buffer[256];
-    char* temp;
+    int lineno=0; int charno=0; int i=0; int spacebreak=0; int jumpno=0;
+    char buffer[128];
+    char* temp; char* temploc;
     if (assembly == 0)
     {
         //fopen returns 0, the NULL pointer, on failure
@@ -74,18 +147,30 @@ int readmips(char* filename){
         exit(-1);
     }
     else {
-        while(fgets(buffer,256,assembly)!=NULL){
+        while(fgets(buffer,128,assembly)!=NULL){
             charno=0;
             while(buffer[charno]!='#' && buffer[charno]!='\n'){
                 charno++;
             } 
             temp = (char*)malloc(charno+1);
+            memset(temp,0,charno+1); // clear new memory
             i=0; spacebreak=0;
             while(isspace(buffer[i])){
                 i++;
             }
             spacebreak=i;
             while(i<charno){
+                if(buffer[i]==':'){ // jump location parse
+                    temploc=(char*)malloc(i+1);
+                    memset(temploc,0,i+1);
+                    strcpy(temploc, temp);
+                    jump_names[jumpno]=temploc; // save jump name
+                    jump_locations[jumpno]=lineno; // and the instruction name
+                    jumpno++;
+                    memset(temp,0,i+1); //reset the memory
+                    while(isspace(buffer[++i])){}
+                    spacebreak+=i;
+                }
                 *(temp+i-spacebreak)=buffer[i];
                 i++;
             }
@@ -97,6 +182,7 @@ int readmips(char* filename){
     }
 }
 
+
 char* getinstruction(int pc){
     return instructions[pc];
 }
@@ -107,9 +193,12 @@ int controllogic(){
     struct j_type j_instr;
     struct i_type i_instr;
     struct r_type r_instr;
-    struct Instruction_Type instr;
+    struct Instruction_Type* instr;
     cur_instruction = getinstruction(pc);
-    instr = *decodeinstruction(cur_instruction);
+    instr = decodeinstruction(cur_instruction);
+    /*if((*instr).instr<25){
+        
+    }*/
     //alu
     //memory read/write
     //register read/write
@@ -118,155 +207,152 @@ int controllogic(){
 }
 
 struct Instruction_Type* decodeinstruction(char* instr){
-    struct j_type a={j, 100123};
-    struct Instruction_Type* ptr = (struct Instruction_Type*) &a;
-    return ptr;
+    char buffer[8]={0};
+    int i=0; int instrnum; int* ptr = &i;
+    struct j_type *j_instr_ptr;
+    struct i_type *i_instr_ptr;
+    struct r_type *r_instr_ptr;
+    struct Instruction_Type* instr_ptr;
+
+    while(*(instr+i)!=' '){
+        buffer[i]=*(instr+i);
+        i++;
+    } 
+    for(instrnum=0;instrnum<44;instrnum++){
+        if(strcmp(buffer,instr_name_list[instrnum])==0){
+            break;
+        }
+    }
+    if(instrnum<25){
+        r_instr_ptr=make_r_type(instrnum, instr, ptr);
+        printf("inum:%d,dest:%d,s1:%d,s2:%d,shamt:%d\n",(*r_instr_ptr).instr,(*r_instr_ptr).dest_register,(*r_instr_ptr).s_register1,(*r_instr_ptr).s_register2,(*r_instr_ptr).shamt);
+        instr_ptr=(struct Instruction_Type*)r_instr_ptr;
+    } else if(instrnum<41){
+        i_instr_ptr=make_i_type(instrnum, instr, ptr);
+        printf("inum:%d,dest:%d,s:%d,imm:%d\n",(*i_instr_ptr).instr,(*i_instr_ptr).dest_register,(*i_instr_ptr).s_register,(*i_instr_ptr).immediate);
+        instr_ptr=(struct Instruction_Type*)i_instr_ptr;
+    } else if (instrnum<43){
+        j_instr_ptr=make_j_type(instrnum, instr, ptr);
+        printf("inum:%d,jump:%d\n",(*j_instr_ptr).instr,(*j_instr_ptr).jump_to);
+        instr_ptr=(struct Instruction_Type*)j_instr_ptr;
+    } else{
+        printf("Instruction not found: %s\n", instr);
+    }
+
+    return instr_ptr;
 }
 
-//ALU function
+struct r_type* make_r_type(int instrnum, char* instr, int* ptr){
+    struct r_type r_instr; 
+    struct r_type *r_type_ptr=&r_instr;
 
-int alu(int operandA, int operandB, int Operation) {
-	int result;
-	if (Operation == "add") {
-		result = add(operandA, operandB);
-	}
-	else if (Operation == "addi") {
-		result = addi(operandA, operandB);
-	}
-	else if (Operation == "addu") {
-		result = addu(operandA, operandB);
-	}
-	else if (Operation == "addiu") {
-		result = addiu(operandA, operandB);
-	}
-	else if (Operation == "sub") {
-		result = sub(operandA, operandB);
-	}
-	else if (Operation == "subu") {
-		result = subu(operandA, operandB);
-	}
-	else if (Operation == "and") {
-		result = and(operandA, operandB);
-	}
-	else if (Operation == "andi") {
-		result = andi(operandA, operandB);
-	}
-	else if (Operation == "or") {
-		result = or(operandA, operandB);
-	}
-	else if (Operation == "ori") {
-		result = ori(operandA, operandB);
-	}
-	else if (Operation == "xor") {
-		result = xor (operandA, operandB);
-	}
-	else if (Operation == "nor") {
-		result = nor(operandA, operandB);
-	}
-	else if (Operation == "slt") {
-		result = slt(operandA, operandB);
-	}
-	else if (Operation == "slti") {
-		result = slti(operandA, operandB);
-	}
-	else if (Operation == "sltu") {
-		result = sltu(operandA, operandB);
-	}
-	else {
-		perror("Command Not Found");
-		exit(-1);
-	}
-	return result;
+    r_instr.instr=instrnum;
+    r_instr.dest_register=nextregister(instr, ptr);
+    if(instrnum<22){
+        r_instr.s_register1=nextregister(instr, ptr);
+        if(instrnum<13){
+            r_instr.s_register2=nextregister(instr, ptr);
+        } else if(instrnum<16){
+            r_instr.shamt=(char)nextint(instr, ptr);
+        }
+    }
+
+    return r_type_ptr;
 }
 
-// Start of Arithmetic functions
-//TODO: Mult and Divide, all operations with 'u' have to use unsigned values, others ahve to check for overflow
-int add(register2, register3) {
-	return RegisterFile[register2] + RegisterFile[register3];
+struct i_type* make_i_type(int instrnum, char* instr, int* ptr){
+    struct i_type i_instr;
+    struct i_type *i_type_ptr=&i_instr;
+    struct indexed_register *indreg_ptr;
+
+    i_instr.instr=instrnum;
+    i_instr.dest_register=nextregister(instr, ptr);
+    if(instrnum==40){
+        i_instr.immediate=nextint(instr, ptr);
+    } else if(instrnum>=32){
+        indreg_ptr = nextindexedregister(instr, ptr);
+        i_instr.s_register=(*indreg_ptr).mem_register; i_instr.immediate=(*indreg_ptr).index;
+    } else{
+        i_instr.s_register=nextregister(instr, ptr); i_instr.immediate=nextint(instr, ptr);
+    }
+
+    return i_type_ptr;
 }
 
-unsigned int addu(register2, register3) {
-	return RegisterFile[register2] + RegisterFile[register3];
+struct j_type* make_j_type(int instrnum, char* instr, int* ptr){
+    struct j_type j_instr;
+    struct j_type *j_type_ptr=&j_instr;
+
+    j_instr.instr=instrnum;
+    j_instr.jump_to=nextint(instr, ptr);
+
+    return j_type_ptr;
 }
 
-int sub(register2, register3) {
-	return RegisterFile[register2] - RegisterFile[register3];
+struct indexed_register* nextindexedregister(char* instr, int* ptr){
+    char reg[4]={0};
+    int i=*ptr; int start; char newit;
+    struct indexed_register indreg;
+    struct indexed_register *indreg_ptr=&indreg;
+    while(isspace(*(instr+i))){i++;}
+    start=i;
+    indreg.index = strtol(instr+start,(char**)NULL,10);
+    while(*(instr+i)!='('){i++;}
+    start=i;
+    while(!isspace(*(instr+i)) && *(instr+i)!=')'){
+        reg[i-start]=*(instr+i);
+        i++;
+    }
+    i = 0;
+    for(newit=0;newit<31;newit++){
+        if(strcmp(reg,RegisterFileNames[newit])==0){
+            indreg.mem_register=newit;
+            i=1;
+            break;
+        }
+    }
+    if(i!=1){indreg.mem_register=32;}
+    return indreg_ptr;
 }
 
-unsigned int subu(register2, register3) {
-	return RegisterFile[register2] - RegisterFile[register3];
+char nextregister(char* instr, int* ptr){
+    char reg[4]={0, 0, 0, 0};
+    int i=*ptr; char newit = 0;
+    while(isspace(*(instr+i))){
+        i++;
+    }
+    while(!isspace(*(instr+i)) && *(instr+i)!=',' && *(instr+i)!=0){
+        reg[newit]=*(instr+i);
+        i++;
+        newit++;
+    }
+    i++; *ptr=i;
+    for(newit=0;newit<31;newit++){
+        if(strcmp(reg,RegisterFileNames[newit])==0){
+            return newit;
+        }
+    }
+    return 32;
 }
 
-int addi(register2, number) {
-	return RegisterFile[register2] + number;
+int nextint(char* instr, int* ptr){
+    int i=*ptr;
+    while(isspace(*(instr+i))){
+        i++;
+    }
+    return (int) strtol(instr+i,(char**)NULL,10);
 }
 
-unsigned int addiu(register2, number) {
-	return RegisterFile[register2] + number;
-}
-
-//Mult and Divide left, have to use LO and HI
-
-// Start of Logical functions
-
-int and(register2, register3) {
-	return (RegisterFile[register2] & RegisterFile[register3]);
-}
-
-int andi(register2, number) {
-	if (RegisterFile[register2] == 1 && number == 1) {
-		return 1;
-	}
-	else {
-		return 0;
-	}
-}
-
-int or(register2, register3) {
-	return (RegisterFile[register2] | RegisterFile[register3]);
-}
-
-int ori(register2, number) {
-	//TODO: Allow assigning to return value
-	if (RegisterFile[register2] == 1 || number == 1) {
-		return 1;
-	}
-	else {
-		return 0;
-	}
-}
-
-int xor(register2, register3) {
-	return (RegisterFile[register2] ^ RegisterFile[register3]);
-}
-
-int nor(register2, register3) {
-	return (~(RegisterFile[register2] | RegisterFile[register3]));
-}
-
-int slt(register2, register3) {
-	if (RegisterFile[register2] < RegisterFile[register3]) {
-		return 1;
-	}
-	else {
-		return 0;
-	}
-}
-
-unsigned int sltu(register2, register3) {
-	if (RegisterFile[register2] < RegisterFile[register3]) {
-		return 1;
-	}
-	else {
-		return 0;
-	}
-}
-
-int slti(register2, number) {
-	if (RegisterFile[register2] < number) {
-		return 1;
-	}
-	else {
-		return 0;
-	}
+// free everything you've malloc'ed
+void america(){
+    int i = 0;
+    // instructions were malloc'ed
+    while(instructions[i]!= NULL){
+        free(instructions[i++]);
+    } 
+    i=0;
+    // jump_names were malloc'ed
+    while(jump_names[i]!= NULL){
+        free(jump_names[i++]);
+    }
 }
